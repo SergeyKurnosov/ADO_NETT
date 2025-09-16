a288e2285cf6239c5032d7809b7122db1cb54d13
SergeyKurnosov <kurnosovsergej123@mail.ru>
2025-09-16 12:04:46 +1000

    Class work 15 09 2025

--- ADO_NETT/Program.cs
+++ ADO_NETT/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//#define SCALAR_CHECK
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,21 +11,71 @@ namespace ADO_NETT
 {
 	internal class Program
 	{
+		static	string connectionString = "Data Source=SERGEY\\MSSQLSERVER17;Initial Catalog=Movies;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+		static SqlConnection connection;
+
 		static void Main(string[] args)
 		{
 			//1 Создаем подключение к базе данных на сервере
-			string connectionString = "Data Source=SERGEY\\MSSQLSERVER17;Initial Catalog=Movies;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 			Console.WriteLine(connectionString);
-			SqlConnection connection = new SqlConnection();
+		    connection = new SqlConnection();
 			connection.ConnectionString = connectionString;
-			//2 Открываем соединение:  После того как подключение создано оно не является открытым тоесть подключение всегда открывается вручную при необходимости
+			//Select("SELECT * FROM Directors");
+			//Select("SELECT * FROM Movies");
+			Select("*", "Directors");
+			Select("movie_name, release_date, first_name+last_name AS Режиссер", "Movies,Directors", "director=director_id");
+#if SCALAR_CHECK
 			connection.Open();
+			string cmd = "SELECT COUNT(*) FROM Directors";
+			SqlCommand command = new SqlCommand(cmd, connection);
+			Console.WriteLine($"Количество режисеров: \t{command.ExecuteScalar()}");
+
+			command.CommandText = "SELECT COUNT(*) FROM Movies";
+			Console.WriteLine($"Количество кино: \t{command.ExecuteScalar()}");
+
+			command.CommandText = "SELECT last_name FROM Directors WHERE first_name = N'James'";
+			Console.WriteLine(command.ExecuteScalar());
+
+
+			connection.Close();
+			Console.WriteLine(Scalar("SELECT last_name FROM Directors WHERE first_name = N'James'"));
+			Console.WriteLine(Scalar("SELECT COUNT(*) FROM Movies"));
+#endif
+
+			Console.Write("Введите имя: ");
+			string first_name = Console.ReadLine();
+
+			Console.Write("Введите фамилию: ");
+			string last_name = Console.ReadLine();
+
+			string cmd = 
+			$"INSERT Directors(director_id,first_name,last_name) VALUES({Convert.ToInt32(Scalar("SELECT MAX(director_id) FROM Directors")) +1},N'{first_name}',N'{last_name}');";
+
+			SqlCommand command = new SqlCommand(cmd, connection);
+			connection.Open();
+			command.ExecuteNonQuery();
+			connection.Close();
+
+			Select("*", "Directors");
+
+		}
+
+
+		static void Select(string fields, string tables, string condition = "")
+		{	
+			//2 Открываем соединение:  После того как подключение создано оно не является открытым тоесть подключение всегда открывается вручную при необходимости
+
+          connection.Open();
 
 			//////////////////////////////////////////////////
 
 
 			//3 Создаем SqlCommand 
-			string cmd = "SELECT * FROM Directors;";
+			string cmd = $"SELECT {fields} FROM {tables}";
+			if (condition != "") cmd += $" WHERE {condition}";
+			cmd += ";";
+
+
 			SqlCommand command = new SqlCommand(cmd, connection);
 
 			//4 создаем Reader
@@ -48,7 +99,17 @@ namespace ADO_NETT
 			//////////////////////////////////////////////////
 			//? !!! Подключения обязательно нужно закрывать !!!
 			connection.Close();
+		}
 
+		static object Scalar(string cmd)
+		{
+			connection.Open();
+
+			SqlCommand command = new SqlCommand(cmd, connection);
+			object obj = command.ExecuteScalar();
+			connection.Close();
+
+			return obj;
 		}
 	}
 }

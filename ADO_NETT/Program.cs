3a63e00bda5b5989c26a6bd3444ef224d8b8c36b
SergeyKurnosov <kurnosovsergej123@mail.ru>
2025-09-17 12:55:42 +1000

    HW 15 09 2025

--- ADO_NETT/Program.cs
+++ ADO_NETT/Program.cs
@@ -1,103 +1,141 @@
-﻿//#define SCALAR_CHECK
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//ado.net
 using System.Data.SqlClient;
 
 namespace ADO_NETT
 {
 	internal class Program
 	{
-		static	string connectionString = "Data Source=SERGEY\\MSSQLSERVER17;Initial Catalog=Movies;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+		static string connectionString = "Data Source=SERGEY\\MSSQLSERVER17;Initial Catalog=Movies;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 		static SqlConnection connection;
 
 		static void Main(string[] args)
 		{
-			//1 Создаем подключение к базе данных на сервере
 			Console.WriteLine(connectionString);
-		    connection = new SqlConnection();
+			connection = new SqlConnection();
 			connection.ConnectionString = connectionString;
-			//Select("SELECT * FROM Directors");
-			//Select("SELECT * FROM Movies");
-			Select("*", "Directors");
-			Select("movie_name, release_date, first_name+last_name AS Режиссер", "Movies,Directors", "director=director_id");
-#if SCALAR_CHECK
-			connection.Open();
-			string cmd = "SELECT COUNT(*) FROM Directors";
-			SqlCommand command = new SqlCommand(cmd, connection);
-			Console.WriteLine($"Количество режисеров: \t{command.ExecuteScalar()}");
 
-			command.CommandText = "SELECT COUNT(*) FROM Movies";
-			Console.WriteLine($"Количество кино: \t{command.ExecuteScalar()}");
+			Insert_2("director_id,first_name,last_name", "Directors", "13,NewDir,Dir");
 
-			command.CommandText = "SELECT last_name FROM Directors WHERE first_name = N'James'";
-			Console.WriteLine(command.ExecuteScalar());
+			//Console.Write("Введите имя: ");
+			//string first_name = Console.ReadLine();
+			//Console.Write("Введите фамилию: ");
+			//string last_name = Console.ReadLine();
+			//int rowsAffected = Insert(first_name, last_name);
 
+			// Select_Parametrs("*", "Directors","first_name", "James");
 
-			connection.Close();
-			Console.WriteLine(Scalar("SELECT last_name FROM Directors WHERE first_name = N'James'"));
-			Console.WriteLine(Scalar("SELECT COUNT(*) FROM Movies"));
-#endif
-
-			Console.Write("Введите имя: ");
-			string first_name = Console.ReadLine();
-
-			Console.Write("Введите фамилию: ");
-			string last_name = Console.ReadLine();
-
-			string cmd = 
-			$"INSERT Directors(director_id,first_name,last_name) VALUES({Convert.ToInt32(Scalar("SELECT MAX(director_id) FROM Directors")) +1},N'{first_name}',N'{last_name}');";
-
-			SqlCommand command = new SqlCommand(cmd, connection);
-			connection.Open();
-			command.ExecuteNonQuery();
-			connection.Close();
-
-			Select("*", "Directors");
+		    	Select("*", "Directors");
 
 		}
 
+		static int Insert(string first_name, string last_name)
+		{
+			string cmd =
+$"INSERT Directors(director_id,first_name,last_name) VALUES({Convert.ToInt32(Scalar("SELECT MAX(director_id) FROM Directors")) + 1},N'{first_name}',N'{last_name}');";
+			SqlCommand command = new SqlCommand(cmd, connection);
+			connection.Open();
+			int rowsAffected = command.ExecuteNonQuery();
+			connection.Close();
 
-		static void Select(string fields, string tables, string condition = "")
-		{	
-			//2 Открываем соединение:  После того как подключение создано оно не является открытым тоесть подключение всегда открывается вручную при необходимости
+			return rowsAffected;
+		}
 
-          connection.Open();
+		static int Insert_2(string fields, string table, string datas) 
+		{
+			connection.Open();
+			string cmd_Insert = $"INSERT {table}({fields}) VALUES(";
+			string cmd = $"SELECT {fields} FROM {table}";
+			string[] values = datas.Split(',') ;
 
-			//////////////////////////////////////////////////
+			SqlCommand command = new SqlCommand(cmd, connection);
+			SqlDataReader reader = command.ExecuteReader();
+			for (int i = 0; i < reader.FieldCount; i++)
+			{
+				switch (Type.GetTypeCode(reader.GetFieldType(i)))
+				{
+					case TypeCode.String:
+						cmd_Insert += $"N'{values[i]}',";
+						break;
+					case TypeCode.Int32:
+						cmd_Insert += $"{values[i]},";
+						break;
+					case TypeCode.DateTime:
+						cmd_Insert += $"{values[i]},";
+						break;
+				}
+			}
+			reader.Close();
+			cmd_Insert = cmd_Insert.Substring(0, cmd_Insert.Length - 1) ;
+			cmd_Insert += ");";
 
+			command.CommandText = cmd_Insert ;
+			int rowsAffected = command.ExecuteNonQuery();
+			connection.Close();
 
-			//3 Создаем SqlCommand 
-			string cmd = $"SELECT {fields} FROM {tables}";
-			if (condition != "") cmd += $" WHERE {condition}";
-			cmd += ";";
+			return rowsAffected;
+		}
+
+		static void Select_Parametrs(string fields, string tables, string search_field, string search = "")
+		{
+			connection.Open();
+			string cmd = $"SELECT {fields} FROM {tables} WHERE {search_field} = @search ;";//
 
 
 			SqlCommand command = new SqlCommand(cmd, connection);
+			command.Parameters.AddWithValue("@search", search);
 
-			//4 создаем Reader
 			SqlDataReader reader = command.ExecuteReader();
-			for (int i = 0;i<reader.FieldCount; i++)
+
+
+			for (int i = 0; i < reader.FieldCount; i++)
 			{
 				Console.Write(reader.GetName(i) + "\t");
 			}
 			Console.WriteLine();
 			while (reader.Read())
 			{
-				//Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");
 				for (int i = 0; i < reader.FieldCount; i++)
 				{
-					Console.Write(reader[i]+"\t\t");
+					Console.Write(reader[i] + "\t\t");
 				}
 				Console.WriteLine();
 			}
 			reader.Close();
+			connection.Close();
+		}
 
-			//////////////////////////////////////////////////
-			//? !!! Подключения обязательно нужно закрывать !!!
+
+//--------------------------------------------------------------------------------------------------------------
+
+		static void Select(string fields, string tables, string condition = "")
+		{
+			connection.Open();
+			string cmd = $"SELECT {fields} FROM {tables}";
+			if (condition != "") cmd += $" WHERE {condition}";
+			cmd += ";";
+
+
+			SqlCommand command = new SqlCommand(cmd, connection);
+			SqlDataReader reader = command.ExecuteReader();
+
+			for (int i = 0; i < reader.FieldCount; i++)
+			{
+				Console.Write(reader.GetName(i) + "\t");
+			}
+			Console.WriteLine();
+			while (reader.Read())
+			{
+				for (int i = 0; i < reader.FieldCount; i++)
+				{
+					Console.Write(reader[i] + "\t\t");
+				}
+				Console.WriteLine();
+			}
+			reader.Close();
 			connection.Close();
 		}
 

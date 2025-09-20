018030758e40fd489f8619fe2b0f8cdfe13d181f
SergeyKurnosov <kurnosovsergej123@mail.ru>
2025-09-20 14:13:56 +1000

    HW not done

--- Academy/Program.cs
+++ Academy/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,15 +9,21 @@ namespace Academy
 {
 	internal static class Program
 	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
+		[DllImport("kernel32.dll")]
+		static extern bool AllocConsole();
+		[DllImport("kernel32.dll")]
+		static extern bool FreeConsole();
+
 		[STAThread]
 		static void Main()
 		{
+			AllocConsole();
+			Console.WriteLine("___");
+
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new MainForm());
+			FreeConsole();
 		}
 	}
 }
